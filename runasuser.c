ac000/runasuser

commit 94e9674ed293b3579a5b864064c64b8fcb41fdf7

Fix a compiler warning
Compiling under gcc version 8.3.1 20190223 (Red Hat 8.3.1-2) (GCC)
produced the following warning

In function ‘do_log’,
    inlined from ‘main’ at runasuser.c:385:8:
runasuser.c:78:3: warning: ‘strncat’ output truncated before terminating nul copying as many bytes from a string as its length [-Wstringop-truncation]
   strncat(cmd, *args, strlen(*args));
   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

Fix this by replacing the strncat(3) and preceding strcat(3) call with a
single call to snprintf(3) which is generally safer to use in any case.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

runasuser.c
@@ -1,7 +1,8 @@
 /*
  * runasuser.c - Run a command as a specified user
  *
- * Copyright (C) 2010 - 2014	Andrew Clayton <andrew@digital-domain.net>
+ * Copyright (C) 2010 - 2014, 2019		Andrew Clayton
+ * 						<andrew@digital-domain.net>
  * Released under the GNU General Public License (GPL) version 2
  * See COPYING
  */
@@ -74,8 +75,7 @@ static int do_log(const char *from_user, const char *to_user, const char *cwd,
 			goto out;
 		}
 		cmd = tmp;
-		strcat(cmd, " ");
-		strncat(cmd, *args, strlen(*args));
+		snprintf(cmd, strlen(*args) + 2, " %s", *args);
 	}
 
 	openlog("runasuser", LOG_ODELAY, LOG_AUTHPRIV);

commit f019574d85976db8d6bb38dfc1fd653f8b453d09

Fix broken command logging
Commit 94e9674 ("Fix a compiler warning") broke the logging of commands
(COMMAND=)

When running something like

    ./runasuser nobody ls -l

we should get the following logged for COMMAND

    COMMAND=/bin/ls -l

however due to the added snprintf() call not taking into account what
was already written into the buffer before adding in the command args
we'd end up with

    COMMAND= -l

Fix is simple. Just track how much data we write into the buffer and use
that as an offset into the buffer for each write.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

runasuser.c
@@ -52,6 +52,7 @@ static int do_log(const char *from_user, const char *to_user, const char *cwd,
 		  const char *cmdpath, char **args)
 {
 	int ret = 1;
+	int len;
 	char *cmd;
 	char *tmp;
 	char *cdn = get_current_dir_name();
@@ -63,7 +64,7 @@ static int do_log(const char *from_user, const char *to_user, const char *cwd,
 		ret = 0;
 		goto out;
 	}
-	snprintf(cmd, strlen(cmdpath) + 1, "%s", cmdpath);
+	len = snprintf(cmd, strlen(cmdpath) + 1, "%s", cmdpath);
 
 	/* Skip past runasuser the user and the command */
 	args += 3;
@@ -75,7 +76,7 @@ static int do_log(const char *from_user, const char *to_user, const char *cwd,
 			goto out;
 		}
 		cmd = tmp;
-		snprintf(cmd, strlen(*args) + 2, " %s", *args);
+		len += snprintf(cmd + len, strlen(*args) + 2, " %s", *args);
 	}
 
 	openlog("runasuser", LOG_ODELAY, LOG_AUTHPRIV);